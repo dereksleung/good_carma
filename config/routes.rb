ed8ee586204a226570d7db86c4a25d54cc44c651
Derek <derek.s.leung.407@gmail.com>
2018-11-20 17:30:43 -0800

    Setup routes for both an Api controller and regular controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :posts do
+        resources :comments
+        resources :inspires, shallow: true, only: [:create, :destroy]
+        resources :inspiractions
+        resources :sympathies
+      end
+
+      resources :users
+      resource :sessions, only: [:new, :create, :destroy]
+    end
+  end
+
+  resources :posts do
+    resources :comments
+    resources :inspires, shallow: true, only: [:create, :destroy]
+    resources :inspiractions
+    resources :sympathies
+  end
+
+  resources :users
+  resource :sessions, only: [:new, :create, :destroy]
+
 end
