73bb92c14e91d3712b068cb9e3414240bdb51244
Derek <derek.s.leung.407@gmail.com>
2018-11-27 11:27:33 -0800

    Fixed seed and Post model errors, committing before making new branch to try project as a one-to-many self-join instead of many-to-many self-join

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,13 @@ class Post < ApplicationRecord
   belongs_to :user
 
   # :parent_relations "names" the PostRelation join table for accessing through the :parent_posts association
-  has_many :parent_relations, foreign_key: parent_post_id, class_name "PostRelation"
+  has_many :parent_relations, foreign_key: parent_post_id, class_name: "PostRelation"
 
   # source: :parent_relation matches with the belongs_to :parent_relation identification in the PostRelation model
   has_many :parent_posts, through: :parent_relations, source: :parent_relation
-
+  
   # :child_relations "names" the PostRelation join table for accessing through the :child_posts association
-  has_many :child_relations, foreign_key: child_post_id, class_name "PostRelation"
+  has_many :child_relations, foreign_key: child_post_id, class_name: "PostRelation"
 
   # source: :child_relation matches with the belongs_to :child_relation identification in the PostRelation model
   has_many :child_posts, through: :child_relations, source:child_relation
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.delete_all
+
 PASSWORD = "supersecret"
 
 super_user = User.create(
   first_name: "Derek",
   last_name: "Leung",
+  email: "derek@leung.com",
+  password: PASSWORD
 )
+
+4.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  u = User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name.downcase}.#{last_name.downcase}@example.com",
+    password: PASSWORD
+  )
+end
+
+users = User.all
+
+10.times do
+  p = Post.create(
+    body: Faker::Hipster.paragraph(2,true,3),
+    user: users.sample
+  )
+end
+
