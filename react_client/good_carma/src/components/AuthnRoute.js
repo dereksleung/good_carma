0e6d8f10ff558a94c4797be5ff54ead7d1e2202c
Derek <derek.s.leung.407@gmail.com>
2018-12-10 16:50:11 -0800

    Made Authenticated Route component. Committing before new branch for this.

--- react_client/good_carma/src/components/AuthnRoute.js
+++ react_client/good_carma/src/components/AuthnRoute.js
@@ -2,14 +2,22 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 const AuthnRoute = props => {
+  const { isAuth, component, ...restProps } = props;
+  const Component = component;
   return(
     <Route
       render={routeProps => {
-
-      }
-
-      }/>
-  )
-}
+        if (isAuth) {
+          return <Component {...routeProps}>
+          </Component>;
+        } else {
+          <Redirect to="session/new"/>
+        }
+      }} 
+      {...restProps}
+    />
+      
+  );
+};
 
 export default AuthnRoute;
