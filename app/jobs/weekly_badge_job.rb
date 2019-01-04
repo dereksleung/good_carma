e7f0bdb85f213e1373de883fbb4ee64c48094c42
Derek <derek.s.leung.407@gmail.com>
2019-01-03 22:22:52 -0800

    Added Overachiever Badge and Weekly Badge Check

--- app/jobs/weekly_badge_job.rb
+++ app/jobs/weekly_badge_job.rb
@@ -4,7 +4,7 @@ class WeeklyBadgeJob < ApplicationJob
   def perform(*args)
     # Do something later
     trailblazer
-    # overachievers(args[0])
+    overachievers
     # muses(args[0])
     # thought_provokers(args[0])
     # wild_growths(args[0])
@@ -43,5 +43,47 @@ class WeeklyBadgeJob < ApplicationJob
   
     end
   end
+
+  def overachievers
+    oachvrs_arr_of_hash = User.find_by_sql("
+      SELECT users.first_name || ' ' || users.last_name, users.avatar, users.id, COUNT(badge_earnings.id) AS Badges
+      FROM users
+      INNER JOIN badge_earnings ON users.id = badge_earnings.user_id
+      INNER JOIN badges ON badge_earnings.badge_id = badges.id 
+      WHERE badge_earnings.created_at > now() - interval '1 week'
+      GROUP BY users.id
+      ORDER BY COUNT(badge_earnings.id) DESC
+      LIMIT 5
+    ")
+    
+    
+    oachvrs_arr_of_hash.each do |oa_user|
+      oa_badge = Badge.find_by_name("Overachievers")
+      badge_earning = BadgeEarning.new(
+        Week: (Time.zone.today - 1.week)..Time.zone.today,
+        Week_s: "#{(Time.zone.today - 1.week).to_formatted_s(:rfc822)} - #{Time.zone.today.to_formatted_s(:rfc822)}"
+        )
+        badge_earning.user = oa_user
+        badge_earning.badge = oa_badge
+        
+      # Save the badge only if the user hasn't earned the same badge this week yet.
+      if oa_user.badge_earnings.any?{|b_e| (b_e.Week === ((Time.zone.today - 1.week)..Time.zone.today)) && (b_e.badge_id = 5)} 
+      else
+        badge_earning.save
+      end
+      
+      
+    end
+  end
+  # Below works for if I need to get repeated earns of the same badge over many weeks.
+
+  # SELECT users.first_name || ' ' || users.last_name, users.avatar, badges.name, badges.image_url, COUNT(badge_earnings.id) AS Badges 
+  # FROM users 
+  # INNER JOIN badge_earnings ON users.id = badge_earnings.user_id
+  # INNER JOIN badges ON badge_earnings.badge_id = badges.id 
+  # WHERE badge_earnings.created_at > now() - interval '4 weeks'
+  # GROUP BY users.id, badges.name, badges.image_url
+  # ORDER BY COUNT(badge_earnings.id) DESC
+  # LIMIT 5
 
 end
