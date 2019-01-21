7d0e56db6316326790d5a954cfe1383837b6e0d2
Derek <derek.s.leung.407@gmail.com>
2019-01-21 11:28:37 -0800

    Got Comments working in AA, registered as Comment instead of PostComment, in a belongs_to inside Post. Used the active_admin initializer to change the AdminComment registration name. Fixed the urlgeneration error by disabling the earlier admin/comment.rb. Inspires now work in AA as belongs_to inside Post.

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -2,7 +2,16 @@ ActiveAdmin.register Post do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :list, :of, :attributes, :on, :model
+  permit_params :list, :of, :attributes, :on, :model
+
+  sidebar "Post Details", only: [:show, :edit, :delete] do
+    ul do
+      li link_to "Comments", admin_post_comments_path(resource)
+      li link_to "Inspires", admin_post_inspires_path(resource)
+    end
+  end
+
+
 #
 # or
 #
@@ -12,4 +21,12 @@ permit_params :list, :of, :attributes, :on, :model
 #   permitted
 # end
 
+end
+
+ActiveAdmin.register Comment do
+  belongs_to :post
+end
+
+ActiveAdmin.register Inspire do
+  belongs_to :post
 end
