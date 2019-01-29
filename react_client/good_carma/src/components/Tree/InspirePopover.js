4d7e0876f9488d9a279aacc935d912c06819f4d1
Derek <derek.s.leung.407@gmail.com>
2019-01-28 23:07:19 -0800

    Tree Popovers showing user avatar and full name. Turned attaching avatar image urls into a method on Post model

--- react_client/good_carma/src/components/Tree/InspirePopover.js
+++ react_client/good_carma/src/components/Tree/InspirePopover.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Tippy from "@tippy.js/react";
 import "tippy.js/dist/tippy.css";
+import UserAvatarSmall from "../UserAvatarSmall";
 
 const InspirePopover = (props) => {
 
@@ -10,8 +11,7 @@ const InspirePopover = (props) => {
     <section className="InspirePopover" style={calcStyle}>
       <Tippy content={
         <>
-          <img src={`${props.avatar}`}>
-          </img> 
+          <UserAvatarSmall avatar_image={props.avatar_image} size="70" /> 
           <p>{props.full_name}</p>
         </>
         } arrow={true} duration={500} delay={[100, 50]} size="large">
